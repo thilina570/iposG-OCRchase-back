5235c44c2d01e45c9fed4a2d4bf33bc254eb51f4
Thilina <thilina570@gmail.com>
2025-01-11 15:46:19 +0530

    test:aws cli testing with s3 upload

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,34 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Aws\S3\S3Client;
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+
+    $s3 = new S3Client([
+        'region'  => env('AWS_DEFAULT_REGION'),
+        'version' => 'latest',
+        'credentials' => [
+            'key'    => env('AWS_ACCESS_KEY_ID'),
+            'secret' => env('AWS_SECRET_ACCESS_KEY'),
+        ],
+    ]);
+
+    $bucket = env('AWS_BUCKET');
+    $filePath = 'file.txt';
+    $key = 'file.txt';
+
+try {
+    $result = $s3->putObject([
+        'Bucket' => $bucket,
+        'Key'    => $key,
+        'SourceFile' => $filePath,
+        // 'ACL'    => 'public-read', // Optional: Set file permissions
+    ]);
+    echo "File uploaded successfully. URL: " . $result['ObjectURL'];
+} catch (Aws\Exception\AwsException $e) {
+    echo "Error uploading file: " . $e->getMessage();
+}
+
 });
